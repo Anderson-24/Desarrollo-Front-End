3d395ab93d7bfb7bb25c7baa539498669a5086fd
Anderson Duarte <Andersonduarte.24@outlook.es>
2020-05-20 13:39:11 -0500

    Animaciones multiples, reto

---
+++ Curso_Animaciones_Web/Proyecto/JS/main.js
@@ -0,0 +1,14 @@
+const $canasta11 = document.getElementById("canastajs11");
+const $pelota5 = document.getElementById('pelotajs5')
+
+// $pelota5.addEventListener('animationstart', (event) => {
+// $pelota5.addEventListener('animationiteration', (event) => {
+$pelota5.addEventListener('animationend', (event) => {
+  if (event.animationName === 'rebote2'){
+    // debugger
+    $pelota5.style.animationName = "skewball"
+    $canasta11.style.alignItems = 'center'
+    $pelota5.style.animationDuration = '5s';
+    $pelota5.style.animationIterationCount = 'infinite'
+  }
+})
